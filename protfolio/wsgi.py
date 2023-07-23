e2c0ce35385aed9b55e86b18ef664172748dfa90
priyankaaurade1 <70333636+priyankaaurade1@users.noreply.github.com>
2023-07-23 09:11:18 +0100

    3rd

--- protfolio/wsgi.py
+++ protfolio/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'protfolio.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
 
 application = get_wsgi_application()
 app = application
